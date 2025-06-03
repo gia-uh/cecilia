c63a043645fb4ad800243aa5b02bbc9c2dbb8a8c
Alejandro Piad <apiad@apiad.net>
2025-06-03 17:02:03 -0400

    Update

--- apps/app.py
+++ apps/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Cecilia - The Cuban Language Model", page_icon="logo.png")
 
+st.image("logo.png", width=300)
+
 patterns = {
     "Download Model from HuggingFace": lambda l,u: st.link_button(label=l, url=u, type="primary", icon="🌟"),
     "Read the Technical Report": lambda l,u: st.link_button(label=l, url="./report", type="secondary", icon="📑"),
